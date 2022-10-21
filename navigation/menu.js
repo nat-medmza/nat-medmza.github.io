9fb95a2bdf3809347a71175473d0e285652f7ca9
nat-medmza <113281636+nat-medmza@users.noreply.github.com>
2022-10-21 01:37:13 -0700

    navigation

---
+++ navigation/menu.js
@@ -0,0 +1,11 @@
+function showmenu(){
+
+  document.getelementbyid('menuoverlay').style.display="block";
+
+}
+
+function hidemenu(){
+
+  document.getelementbyid('menuoverlay').style.display="none";
+
+}
