b53891a134586e195733a152af69c1181a2c1495
nat-medmza <113281636+nat-medmza@users.noreply.github.com>
2022-10-28 00:27:34 -0700

    components

---
+++ animatingcomponents/menu.js
@@ -0,0 +1,13 @@
+function showmenu(){
+
+  console.log(document.getElementById('menuoverlay'));
+  document.getElementById('menuoverlay').style.display="block";
+
+
+}
+
+function hidemenu(){
+
+  document.getElementById('menuoverlay').style.display="none";
+
+}
