dcbf360234373fc633566ec188b3dd9e03d50674
nat-medmza <113281636+nat-medmza@users.noreply.github.com>
2022-10-31 01:23:40 -0700

    pageplusone

---
+++ menubar/menu.js
@@ -0,0 +1,14 @@
+
+function showmenu(){
+
+  console.log(document.getElementById('menuoverlay'));
+  document.getElementById('menuoverlay').style.display="block";
+
+
+}
+
+function hidemenu(){
+
+  document.getElementById('menuoverlay').style.display="none";
+
+}
